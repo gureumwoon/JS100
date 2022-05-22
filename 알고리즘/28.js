36b9f1b6025dc7f794ef94e58a3049a962fa4414
gureumwoon <vfrvfr3221@gmail.com>
2022-05-22 22:32:20 +0900

    5/22 다른풀이

--- 알고리즘/28.js
+++ 알고리즘/28.js
@@ -23,3 +23,13 @@ function solution(x) {
     }
     return x % answer === 0 ? true : false;
 }
+
+function solution(x) {
+    let num = x;
+    let sum = 0;
+    do {
+        sum += x % 10;
+        x = Math.floor(x / 10);
+    } while (x > 0);
+    return !(num % sum);
+}
